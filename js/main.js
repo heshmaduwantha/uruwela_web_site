b408080772a0035fe5ef05075ee4eab9c71450ed
heshmaduwantha <heshan5maduwantha@gmail.com>
2024-08-06 13:59:30 +0530

    new update

--- js/main.js
+++ js/main.js
@@ -18,8 +18,8 @@ $(document).ready(function () {
                 items: 3
             }
         }
+       
     })
-
 
 });
 
@@ -75,3 +75,54 @@ $(document).ready(function () {
 
 });
 
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    fetch('./common/navbar.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('navbar-container').innerHTML = data;
+        })
+        .catch(error => console.error('Error loading navbar:', error));
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const path = window.location.pathname;
+    console.log('Current Path:', path);
+    const navLinks = {
+        '/our-story.html': 'our-story',
+        '/our-team.html': 'our-team',
+        '/process.html': 'process',
+        '/products.html': 'products',
+        '/gallery.html': 'gallery'
+    };
+
+    fetch('./common/navbar.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('navbar-container').innerHTML = data;
+
+            Object.keys(navLinks).forEach(key => {
+                const element = document.getElementById(navLinks[key]);
+                if (element) {
+                    if (path.includes(key)) {
+                        element.classList.add('nav-active');
+                    } else {
+                        element.classList.remove('nav-active');
+                    }
+                }
+            });
+        })
+        .catch(error => console.error('Error loading navbar:', error));
+});
+
+window.addEventListener("scroll", function() {
+    var navbar = document.getElementById("navbar");
+    if (window.pageYOffset >= 10) {
+      console.log('Navbar is sticky');
+      navbar.classList.add("sticky");
+    } else {
+      console.log('Navbar is stickm m y');
+      navbar.classList.remove("sticky");
+    }
+})
